c4eab307031da11c34e8e161cf8ddea7a54cbe88
hlyl <hlyl@novonordisk.com>
2023-12-16 10:50:41 +0100

    first commit

---
+++ Home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import streamlit as st
+
+st.set_page_config(
+    page_title="VtxGaming",
+    page_icon="👋",
+)
+
+st.write("# Welcome to Vtxgaming! 👋")
+
+st.sidebar.success("Select a Game above.")
+
+st.markdown(
+    """
+    Vtxgaming is a website where you can play games with your friends.
+    **👈 Select a game from the sidebar** to see some examples
+    of what you are able to control here!
+"""
+)
---
+++ pages/csgo2.py
@@ -0,0 +1,66 @@
+import streamlit as st
+import configparser
+from rcon.source import Client
+
+
+# Your existing functions
+def run_rcon_command(command, *args):
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+
+    ip = config["RCON"]["ip"]
+    port = int(config["RCON"]["port"])
+    password = config["RCON"]["password"]
+    with Client(ip, port, passwd=password) as client:
+        # Convert all arguments to strings
+        str_args = [str(arg) for arg in args]
+        response = client.run(command, *str_args)
+    return response
+
+
+# Streamlit app
+def main():
+    st.title("Counterstrike2")
+
+    # Dropdown for map selection
+    map_name = st.selectbox(
+        "Select a map:",
+        [
+            "de_dust2",
+            "de_mirage",
+            "de_inferno",
+            "de_overpass",
+            "de_nuke",
+            "de_train",
+            "de_vertigo",
+        ],
+    )
+
+    # Button for execution
+    if st.button("Change Map"):
+        response = run_rcon_command("changelevel", map_name)
+        st.success(f"Response: {response}")
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        # Create a button to remove bots
+        if st.button("Remove Bots"):
+            response = run_rcon_command("bot_kick")
+            st.success(f"Response: {response}")
+
+    with col2:
+        # Create a button to add bots to ct
+        if st.button("Add CT Bots"):
+            response = run_rcon_command("bot_add_ct expert")
+            st.success(f"Response: {response}")
+
+    with col3:
+        # Create a button to add bots to t
+        if st.button("Add T Bots"):
+            response = run_rcon_command("bot_add_t expert")
+            st.success(f"Response: {response}")
+
+
+if __name__ == "__main__":
+    main()
